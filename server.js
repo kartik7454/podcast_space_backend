d4150db11ca025be1a7e5b95d79ead3de2de9675
kartik7454 <105301383+kartik7454@users.noreply.github.com>
2024-02-02 22:35:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongDBSession = connectMongoDBSession(session);
 
 const app = express()
 app.use(cors())
-app.set("trust proxy", 1); 
+
 
 const store = new mongDBSession({
     uri:process.env.MONGO_URI,
@@ -21,6 +21,7 @@ app.use(session( {
     resave:false,
     saveUninitialized:true,
     store:store,
+    proxy: true,
     cookie: {
          // required for cookies to work on HTTPS
         secure: true,
