4b7809144d7689cd5e194d9efac38676b5980c9d
kartik7454 <105301383+kartik7454@users.noreply.github.com>
2024-02-02 18:18:24 +0530

    Create sessions.js

---
+++ models/sessions.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema
+
+const todoSchema = new Schema({
+
+    
+ expires:{}
+       
+        
+       })
+          
+        
+        
+      
+
+
+
+
+    
+
+       const session = mongoose.model("mySession", todoSchema);
+
+       export default session
